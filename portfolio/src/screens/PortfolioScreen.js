b9ad28ebfce9b8fd50c01e0f5a21f5c4ba4764b0
franciscaluz <vfranciscaluz@gmail.com>
2020-11-08 15:49:05 -0500

    fix portfolio collapse

--- portfolio/src/screens/PortfolioScreen.js
+++ portfolio/src/screens/PortfolioScreen.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from "react";
+import React from "react";
 import styled from "styled-components/macro";
 import BaseScreen from "./BaseScreen";
 import { Container } from "reactstrap";
